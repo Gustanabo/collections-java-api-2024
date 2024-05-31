eb66d62e879fec5dfc3be86d1963b0d7d7c445b7
Gustanabo <xgustanabo@gmail.com>
2024-05-31 11:54:57 -0300

    Resolvendo desafio OrdenacaoNumeros

---
+++ src/main/java/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,65 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    private List<Integer> listaNumeros;
+
+    public OrdenacaoNumeros() {
+        this.listaNumeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        listaNumeros.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.listaNumeros);
+        if (!listaNumeros.isEmpty()) {
+            Collections.sort(numerosAscendente);
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numerosDescendente = new ArrayList<>(this.listaNumeros);
+        if (!listaNumeros.isEmpty()) {
+            numerosDescendente.sort(Collections.reverseOrder());
+            return numerosDescendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros() {
+        if (!listaNumeros.isEmpty()) {
+            System.out.println(this.listaNumeros);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarAscendente());
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarDescendente());
+
+        numeros.exibirNumeros();
+    }
+}
